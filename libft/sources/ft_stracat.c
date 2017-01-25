d552aa83465af4c90a51faf8823d9f637aae024d
mo0k <julien.moucadel@gmail.com>
2017-01-26 00:08:53 +0100

    del ft_putstr

--- libft/sources/ft_stracat.c
+++ libft/sources/ft_stracat.c
@@ -6,7 +6,7 @@
 /*   By: mo0ky <mo0ky@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/10 20:53:17 by jmoucade          #+#    #+#             */
-/*   Updated: 2017/01/12 21:56:49 by mo0ky            ###   ########.fr       */
+/*   Updated: 2017/01/25 22:20:05 by mo0ky            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int			ft_stracat(char **dst, char *src)
 {
-	char			*tmp;
+	char	*tmp;
 
 	if (!dst || !src || !*dst)
 		return (0);
--- libft/sources/ft_strjoin.c
+++ libft/sources/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mo0ky <mo0ky@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/05 14:40:49 by jmoucade          #+#    #+#             */
-/*   Updated: 2017/01/24 11:16:22 by mo0ky            ###   ########.fr       */
+/*   Updated: 2017/01/25 22:19:46 by mo0ky            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ char		*ft_strjoin(char const *s1, char const *s2)
 	ptr = NULL;
 	if (s1 && s2)
 	{
-		ft_putstr(s1);
-		ft_putstr(s2);
 		if ((ptr = ft_strnew(ft_strlen(s1) + ft_strlen(s2) + 1)))
 		{
 			ptr = ft_strcpy(ptr, s1);
