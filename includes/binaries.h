e90ae47987f03109f663dab2662b4d9a36711ee3
mo0k <julien.moucadel@gmail.com>
2017-01-26 00:03:39 +0100

    add two header files

---
+++ includes/binaries.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   binaries.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mo0ky <mo0ky@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/25 23:59:22 by mo0ky             #+#    #+#             */
+/*   Updated: 2017/01/26 00:01:46 by mo0ky            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BINARIES_H
+# define BINARIES_H
+
+#include <minishell.h>
+
+int			do_exec(t_list *environ, t_cmd cmd);
+t_cmd		get_path(char *env_path, char **cmd, t_list *env);
+
+#endif
