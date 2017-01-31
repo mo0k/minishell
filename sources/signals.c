53fdcdb248a3e59e8bbceaedc35068874d529308
Julien MOUCADEL <jmoucade@e2r6p1.42.fr>
2017-01-31 16:17:58 +0100

    norme

--- sources/signals.c
+++ sources/signals.c
@@ -6,17 +6,17 @@
 /*   By: jmoucade <jmoucade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/31 14:32:51 by jmoucade          #+#    #+#             */
-/*   Updated: 2017/01/31 14:59:57 by jmoucade         ###   ########.fr       */
+/*   Updated: 2017/01/31 16:17:22 by jmoucade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signals.h>
 
-void		handler_prompt(int sig)
+void		handler_prompt(int signo)
 {
-	(void)sig;
+	(void)signo;
 	write(1, "\n", 1);
-	ft_putstr(C_PROMPT);
+	prompt();
 }
 
 void		handler_noprompt(int signo)
