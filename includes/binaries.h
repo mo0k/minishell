754259364063b12dd0595cd6b8d9872cbfbe0573
mo0k <julien.moucadel@gmail.com>
2017-02-05 01:17:04 +0100

    modif check_cmd for quote

--- includes/binaries.h
+++ includes/binaries.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   binaries.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmoucade <jmoucade@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mo0ky <mo0ky@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/25 23:59:22 by jmoucade          #+#    #+#             */
-/*   Updated: 2017/01/31 15:01:57 by jmoucade         ###   ########.fr       */
+/*   Updated: 2017/02/05 01:02:47 by mo0ky            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ int			do_exec(t_list *environ, t_cmd cmd);
 int			do_fork(char *binpath, char**av, char **env);
 t_cmd		get_binpath(char *env_path, char **cmd, t_list *env);
 char		**check_cmds(char **cmd, t_list *env);
-void		check_cmd(char **cmd, t_list *env);
+char		*check_cmd(char *cmd, t_list *env);
 
 #endif
